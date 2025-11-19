4668297e4cf0d1690d2e258d147e22b09757a599
Michael Silva <michaelsantospigs@gmail.com>
2025-11-19 10:53:25 -0300

    corigindo API_URL e adicionando API_URL_AVATAR

--- classroom/src/components/NavBar.tsx
+++ classroom/src/components/NavBar.tsx
@@ -62,11 +62,11 @@ export function Navbar() {
 
 
 
-  const API_URL = import.meta.env.VITE_API_URL.replace(/\/+$/, "");
-
+  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL_AVATAR = import.meta.env.VITE_API_URL.replace(/\/+$/, "");
   const defaultAvatar = "/assets/Avatar.png"; // pasta public do frontend
   const avatarToShow = userPhoto
-    ? (userPhoto.startsWith("http") ? userPhoto : `${API_URL}${userPhoto}`)
+    ? (userPhoto.startsWith("http") ? userPhoto : `${API_URL_AVATAR}${userPhoto}`)
     : defaultAvatar;
 
 
